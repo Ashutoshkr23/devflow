33d379693cf9561f4a41cf073775e36b90cc7866
Ashutosh Kumar <ashutoshkumar.ash205@gmail.com>
2024-02-19 21:40:42 +0530

    Add Auth

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,11 +1,9 @@
-import React from 'react'
+import { UserButton } from "@clerk/nextjs"
 
-const Home = () => {
-  return (
+export default function Home(){
+  return(
     <div>
-      <h1>Hello</h1>
+      <UserButton afterSignOutUrl="/"/>
     </div>
   )
 }
-
-export default Home
