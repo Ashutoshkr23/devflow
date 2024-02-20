90114c96783524b077099eb50c39373d8664c57a
Ashutosh Kumar <ashutoshkumar.ash205@gmail.com>
2024-02-21 01:27:50 +0530

    Side bar completed

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,5 @@
+import LeftSidebar from '@/components/shared/LeftSidebar'
+import RightSidebar from '@/components/shared/RightSidebar'
 import Navbar from '@/components/shared/navbar/Navbar'
 import React from 'react'
 
@@ -6,14 +8,15 @@ const Layout = ({ children } : {children: React.ReactNode}) => {
     // eslint-disable-next-line tailwindcss/no-custom-classname
     <main className="background-light850_dark100 relative">
       <Navbar/>
-      <div className='flex'>
-        leftsidebar
+      <div className='flex '>
+       <LeftSidebar/>
         <section className='flex min-h-screen flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14'>
             <div className="mx-auto w-full max-w-5xl">
+              hello hi bye
                 {children}
             </div>
         </section>
-        RightSideBar
+        <RightSidebar/>
       </div>
       Toaster
     </main>
