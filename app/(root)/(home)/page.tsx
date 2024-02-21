17a6852992305e17e262a8d642b574709a7653cc
Ashutosh Kumar <ashutoshkumar.ash205@gmail.com>
2024-02-21 16:24:55 +0530

    ui fix for homepage

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -8,7 +8,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+      <div className="flex w-full  flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
         <h1 className="h1-bold text-dark100_light900">All Questions</h1>
         <Link href="/ask-question">
           <Button className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900">
@@ -16,7 +16,7 @@ export default function Home() {
           </Button>
         </Link>
       </div>
-      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+      <div className="mt-11 flex flex-col justify-start gap-5  sm:items-center">
         <LocalSearch
           route="/"
           iconposition="left"
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,26 +1,23 @@
-import LeftSidebar from '@/components/shared/LeftSidebar'
-import RightSidebar from '@/components/shared/RightSidebar'
-import Navbar from '@/components/shared/navbar/Navbar'
-import React from 'react'
+import LeftSidebar from "@/components/shared/LeftSidebar";
+import RightSidebar from "@/components/shared/RightSidebar";
+import Navbar from "@/components/shared/navbar/Navbar";
+import React from "react";
 
-const Layout = ({ children } : {children: React.ReactNode}) => {
+const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     // eslint-disable-next-line tailwindcss/no-custom-classname
     <main className="background-light850_dark100 relative">
-      <Navbar/>
-      <div className='flex '>
-       <LeftSidebar/>
-        <section className='flex min-h-screen flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14'>
-            <div className="mx-auto w-full max-w-5xl">
-              hello hi bye
-                {children}
-            </div>
+      <Navbar />
+      <div className="flex ">
+        <LeftSidebar />
+        <section className="flex min-h-screen w-full flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
+          <div className="mx-auto w-full max-w-5xl">{children}</div>
         </section>
-        <RightSidebar/>
+        <RightSidebar />
       </div>
       Toaster
     </main>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
