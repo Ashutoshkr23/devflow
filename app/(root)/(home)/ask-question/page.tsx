ef3f8b4f51b4c0e6ddf24deb8031419bf2fea81a
Ashutosh Kumar <ashutoshkumar.ash205@gmail.com>
2024-03-04 23:07:49 +0530

    user action created

--- app/(root)/(home)/ask-question/page.tsx
+++ app/(root)/(home)/ask-question/page.tsx
@@ -1,15 +1,26 @@
-import Question from '@/components/forms/Question'
-import React from 'react'
+import Question from "@/components/forms/Question";
+import { getUserById } from "@/lib/actions/user.action";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import React from "react";
 
-const page = () => {
+const Page = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
+  const mongoUser = await getUserById({ userId });
+
   return (
     <div>
-      <h1 className='h1-bold text-dark100_light900'>Ask Question</h1>
-      <div className='mt-9'>
-        <Question/>
+      <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
+      <div className="mt-9">
+        <Question mongoUserId={JSON.stringify(mongoUser?._id)} />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default Page;
